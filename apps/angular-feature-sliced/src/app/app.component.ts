084df3b2e948b8daf50f8d8a3bdf04f6a0028d2e
Maxim Kokosha <kokosha.maxim@gmail.com>
2021-12-29 00:51:07 +0200

    initial commit

---
+++ apps/angular-feature-sliced/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-feature-sliced-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-feature-sliced';
+}
