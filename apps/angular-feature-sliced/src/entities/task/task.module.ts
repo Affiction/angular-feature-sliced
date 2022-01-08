b60cfdef3b056e820d3702468a7812aaf51b9027
Maxim Kokosha <kokosha.maxim@gmail.com>
2022-01-09 01:32:29 +0200

    chore: remove commented code

--- apps/angular-feature-sliced/src/entities/task/task.module.ts
+++ apps/angular-feature-sliced/src/entities/task/task.module.ts
@@ -2,22 +2,12 @@ import { NgModule } from '@angular/core';
 import { MaterialModule, SharedModule } from 'shared/modules';
 
 import { TaskCardComponent, TaskRowComponent } from './components';
-// import { StoreModule } from '@ngrx/store';
-// import { EffectsModule } from '@ngrx/effects';
-// import * as fromTask from './store/task.reducer';
-// import { TaskEffects } from './store/task.effects';
-// import { TaskFacade } from './store/task.facade';
 
 const EXPORT_COMPONENTS = [TaskCardComponent, TaskRowComponent];
 
 @NgModule({
   declarations: EXPORT_COMPONENTS,
-  imports: [
-    SharedModule,
-    MaterialModule,
-    // StoreModule.forFeature(fromTask.TASK_FEATURE_KEY, fromTask.reducer),
-    // EffectsModule.forFeature([TaskEffects]),
-  ],
+  imports: [SharedModule, MaterialModule],
   exports: [...EXPORT_COMPONENTS],
 })
 export class TaskModule {}
